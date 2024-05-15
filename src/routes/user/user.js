1d5c36b1564d8dd51c87e8f1149c22f11d7b047d
Elias Josué HAJJAR LLAUQUEN <elias-josue.hajjar-llauquen@epitech.eu>
2024-05-15 22:38:53 +0200

    [ADD] update user done.

--- src/routes/user/user.js
+++ src/routes/user/user.js
@@ -85,5 +85,34 @@ user_id.get("/", authenticateToken, (req, res) => {
     }
 });
 
+user_id.put("/", authenticateToken, (req, res) => {
+    id = req.params.IDOrEmail;
+
+    if (!id) return res.status(400).json({msg: "Bad parameter"});
+    if (isNaN(id)) return res.status(400).json({msg: "Bad parameter"});
+    if (!req.body.email || !req.body.password || !req.body.name || !req.body.firstname)
+        return res.status(400).json({msg: "Bad parameter"});
+    if (typeof req.body.email !== "string" || typeof req.body.password !== "string" || typeof req.body.name !== "string" || typeof req.body.firstname !== "string")
+        return res.status(400).json({msg: "Bad parameter"});
+
+    db.query('SELECT * FROM user WHERE id = ?', [id], (err, result) => {
+        if (result.length <= 0) return res.status(404).json({msg: "Not Found"});
+
+        const user = {
+            email: req.body.email,
+            password: crypt.hashSync(req.body.password, 2),
+            name: req.body.name,
+            firstname: req.body.firstname
+        };
+
+        db.query('UPDATE user SET ? WHERE id = ?', [user, id], (err, result) => {
+            if (err) {
+                return res.status(500).json({msg: "Internal Server Error"});
+            }
+            return res.status(200).json({msg: "User updated !"});
+        });
+    });
+});
+
 
 module.exports = { user, user_id };
