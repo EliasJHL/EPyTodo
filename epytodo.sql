de4d339215b48f1951888f4d49fc5d3b9e12824d
Elias Josué HAJJAR LLAUQUEN <elias-josue.hajjar-llauquen@epitech.eu>
2024-05-16 11:37:49 +0200

    [FIX] epytodo.sql fixed

--- epytodo.sql
+++ epytodo.sql
@@ -11,11 +11,11 @@ USE epytodo;
 CREATE TABLE IF NOT EXISTS user
 (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
     firstname VARCHAR(255) NOT NULL,
-    created_at DATETIME
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS todo
@@ -23,8 +23,9 @@ CREATE TABLE IF NOT EXISTS todo
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
-    created_at DATETIME,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     due_time DATETIME NOT NULL,
-    status ENUM('not started', 'todo', 'in progress', 'done') NOT NULL,
-    user_id INT NOT NULL
+    status ENUM('not started', 'todo', 'in progress', 'done') NOT NULL DEFAULT 'not started',
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id)
 );
